81dafdb81792d025d29f7b58f252bb887b016627
Francho Garcia <franchog79@gmail.com>
2019-07-24 13:41:17 +0200

    First commit

---
+++ client/src/Services/Services.js
@@ -0,0 +1,30 @@
+import Axios from "axios";
+
+export default class AuthServices {
+  constructor() {
+    this.service = Axios.create({
+      baseURL: "http://localhost:3000/auth",
+      withCredentials: true
+    });
+  }
+
+  signup = (username, password) => {
+    return this.service
+      .post("/signup", { username, password })
+      .then(response => response.data);
+  };
+
+  login = (username, password) => {
+    return this.service
+      .post("/login", { username, password })
+      .then(response => response.data);
+  };
+
+  loggedin = () => {
+    return this.service.get("/userData").then(response => response.data);
+  };
+
+  logout = () => {
+    return this.service.get("/logout").then(response => response.data);
+  };
+}
