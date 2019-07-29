dc82833c30544d36bd9a8d03a0951fa4611973eb
Francho Garcia <franchog79@gmail.com>
2019-07-29 17:53:17 +0200

    added city select

--- client/src/components/ProfileUser/Profile.js
+++ client/src/components/ProfileUser/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./Profile.css";
+import SearchCity from "../SearchCity/SearchCity";
 
 export default class Profile extends Component {
   constructor() {
@@ -44,6 +45,11 @@ export default class Profile extends Component {
     // then we can use axios to communicate with our API to record this data for example
     // axios.put("http://localhost:3000/employeeDetails", this.state.userFormDetails).then
   }
+
+  citySelected(city) {
+    this.setState({ ...this.state, city: city.value });
+  }
+
   render() {
     return (
       <div className="profile-form">
@@ -51,29 +57,38 @@ export default class Profile extends Component {
           <h1>Welcome to your profile: {this.props.username} </h1>
           <fieldset className="fieldset-wrapper">
             <input
+              className="fields-of-form"
               type="text"
               placeholder="please write your username"
               value={this.state.userFormDetails.username}
               onChange={e => this.handleFormChange(e, "username")}
             />
             <input
+              className="fields-of-form"
               type="password"
               placeholder="please write your password"
               value={this.state.userFormDetails.password}
               onChange={e => this.handleFormChange(e, "password")}
             />
             <input
+              className="fields-of-form"
               type="text"
               placeholder="please write your experience"
               value={this.state.userFormDetails.experience}
               onChange={e => this.handleFormChange(e, "experience")}
             />
-            <input
+            {/* <input
               type="text"
               placeholder="please write your city"
               value={this.state.userFormDetails.age}
               onChange={e => this.handleFormChange(e, "city")}
-            />
+            /> */}
+            <SearchCity
+              className="fields-of-form"
+              filterCity={e => this.citySelected(e)}
+            >
+              {this.state.city}>
+            </SearchCity>
           </fieldset>
           <button onClick={e => this.submitForm(e)}>Submit this form!</button>
         </form>
