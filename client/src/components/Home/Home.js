14fe52fac7c0f28041cde7a910a8507444d5d4b2
Francho Garcia <franchog79@gmail.com>
2019-08-01 20:25:39 +0200

    created a intro

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -12,6 +12,13 @@ export default class Home extends Component {
             src={backgroundPict}
             alt="Background App"
           />
+          <p className="text-intro">
+            MakeUp&amp;Call es una web que permite ponerte en contacto con los
+            artistas del maquillaje de tu ciudad, para que vayan a tu casa
+            cuando los necesites. Filtra por los servicios que estés buscando,
+            consulta las opiniones de nuestros usuarios y encuentra a uno de los
+            muchos artistas que tenemos a tu disposición.
+          </p>
         </div>
       </React.Fragment>
     );
